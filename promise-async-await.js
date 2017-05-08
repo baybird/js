a62d149d492c48db92e26d89c82c91a3d85bcf07
Robert Tang <bayareabird@gmail.com>
2017-05-08 11:41:11 -0700

    Improved commen

--- promise-async-await.js
+++ promise-async-await.js
@@ -3,39 +3,30 @@
 // Date   : 5/6/2017
 
 function httpGet(url){
-    // XMLHttpRequest is an API that provides client functionality for transferring data between a client and a server.
-    // It provides an easy way to retrieve data from a URL without having to do a full page refresh.
-
+    
     return new Promise((resolve, reject)=>{
       // Load XMLHttpRequest module
+      // XMLHttpRequest provides an easy way to retrieve data from a URL without having to do a full page refresh.
       var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
       // instantiation
       var req = new XMLHttpRequest();
 
-      // Initializes a request.
+      // initializes a request.
       req.open('GET', url);
 
       // Assign funciton for HTTP request returns after successfully fetching content
       // and the load event is received by this object.
       req.onload = function() {
-        // This is called even on 404 etc
-        // so check the status
+        // check the status
         if (req.status == 200) {
           // Resolve the promise with the response text
-          // console.log(req);
           resolve(req);
         }else {
           // Otherwise reject with the status text
-          // which will hopefully be a meaningful error
           reject(Error(req.status));
-          // console.log(req.status)
         }
       };
-
-      // req.onprogress = function(event){
-      //   console.log(event)
-      // };
 
       // Handle network errors
       req.onerror = function() {
@@ -43,7 +34,7 @@ function httpGet(url){
       };
 
 
-      // Make the request
+      // make the request
       req.send();
     })
 
